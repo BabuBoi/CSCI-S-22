569f5bd1a5aa62417f7369036080c6233348d727
BabuBoi <george452314@gmail.com>
2025-07-14 08:56:23 +0400

    added extra base cases cause boof lol foo

--- ps1/Problem4.java
+++ ps1/Problem4.java
@@ -4,6 +4,12 @@ public class Problem4 {
         if (i == (arr.length - 1)){
             System.out.println(arr[i]);
         }
+        else if (i >= arr.length || i < 0){
+            throw new IllegalArgumentException("index out of bounds");
+        }
+        else if (arr.length == 0){
+            System.out.println("{}");
+        }
         else{
             printReverse(arr, i + 1);
             System.out.println(arr[i]);
@@ -15,5 +21,7 @@ public class Problem4 {
     public static void main(String[] args){
         Object[] items = {"a" , "b" , "c"};
         printReverse(items);
+        Object[] g = {};
+        printReverse(g);
     }
 }
